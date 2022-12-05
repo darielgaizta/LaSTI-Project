e0c0796f3e917a340293f75efc7980ca5ba578ad
Dariel <80236164+darielgaizta@users.noreply.github.com>
2022-12-05 14:31:43 +0700

    updates for deployment

--- lasti_project/settings.py
+++ lasti_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*je*=n8f!kwu5f7g2d^*=a#ug8p!eie_%#8^qlt+1&h+t_wkh*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
