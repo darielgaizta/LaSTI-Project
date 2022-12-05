9cea9da6da5fd108704b7ebc9997b124c8799712
Dariel <80236164+darielgaizta@users.noreply.github.com>
2022-12-05 15:37:42 +0700

    debug staticfiles

--- lasti_project/settings.py
+++ lasti_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*je*=n8f!kwu5f7g2d^*=a#ug8p!eie_%#8^qlt+1&h+t_wkh*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,10 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ staticfiles/js/comment.js
@@ -0,0 +1,17 @@
+$(document).on('submit', '#comment-form', function(e) {
+	e.preventDefault();
+	$.ajax({
+		type: 'POST',
+		url : '/course/sync-session/stream/comments/submit/',
+		data: {
+			username: $('#username').val(),
+			text: $('#text').val(),
+			csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+		},
+		success: function(data) {
+			alert(data)
+		}
+	});
+	document.getElementById('username').value = '';
+	document.getElementById('text').value = '';
+})
