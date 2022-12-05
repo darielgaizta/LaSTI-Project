5e53ff872ca3d55cc084503674153856dceab71b
Dariel <80236164+darielgaizta@users.noreply.github.com>
2022-12-05 15:58:35 +0700

    switch dbms

--- lasti_project/settings.py
+++ lasti_project/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'lasti_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'sVbsZ3W1Pa8pEaFBPIWe',
+        'HOST': 'containers-us-west-137.railway.app',
+        'PORT': '6293'
     }
 }
 
